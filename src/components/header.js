c7b20b86b17d8efc3de9c71424df7f93a1910dcc
VIHANGA <vihanga2003nimsara@gmail.com>
2024-05-22 10:33:35 +0530

    configuered all dependencies

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,8 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
-      display: `flex`,
-      alignItems: `center`,
-      justifyContent: `space-between`,
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      {siteTitle}
-    </Link>
+  <header>
+    <Link to="/">{siteTitle}</Link>
     <img
       alt="Gatsby logo"
       height={20}
